21a476db2b5bdd8771ee0ecca992fd31e55aadbe
Amzad100 <mdamzad2003a@gmail.com>
2023-03-03 02:32:38 +0600

    spinner added

--- js/index.js
+++ js/index.js
@@ -1,16 +1,24 @@
-const loadData = async() => {
+const loadData = async () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
     displayData(data.data.tools);
 }
-const displayData = datas =>{
+const displayData = datas => {
     const dataContainer = document.getElementById('data-container');
+    const seeMore = document.getElementById('seeMore');
+    if(datas.length > 6){
+        datas = datas.slice(0, 6);
+        seeMore.classList.remove('d-none')
+    }
+    else{
+        seeMore.classList.add('d-none');
+    }
     datas.forEach(data => {
         const dataDiv = document.createElement('div');
         dataDiv.classList.add('col');
         dataDiv.innerHTML = `
-        <div class="card p-4 h-100">
+        <div class="card p-3 h-100">
         <img src="${data.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Features</h5>
@@ -20,19 +28,30 @@ const displayData = datas =>{
                 <li>${data}</li>
             </ol>
         <hr>
-        <div class="">
-            <div>
-            <h5>${data.name}</h5>
-            <p>${data.published_in}</p>
+        <h5>${data.name}</h5>
+        <div class="row justify-content-between">
+            <div class="col-4">
+            <p><i class="fa-solid fa-calendar-days"></i> ${data.published_in}</p>
             </div>
-            <div>
+            <div class="col-2">
+            <button><i class="fa-sharp fa-solid fa-arrow-right"></i></button>
             </div>
         </div>
         </div>
     </div>
         `;
         dataContainer.appendChild(dataDiv);
+        togleSpinner(false);
     });
 };
-
+const togleSpinner = isLoading => {
+    const lodarSection = document.getElementById('loader');
+    if (isLoading) {
+        lodarSection.classList.remove('d-none')
+    }
+    else{
+        lodarSection.classList.add('d-none');
+    }
+}
+togleSpinner(true);
 loadData();
