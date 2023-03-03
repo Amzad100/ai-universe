9ea42778e8306749e302ddd8b3f7bd57815e3f62
Amzad100 <mdamzad2003a@gmail.com>
2023-03-03 14:23:14 +0600

    see more button work done

--- js/index.js
+++ js/index.js
@@ -7,11 +7,11 @@ const loadData = async () => {
 const displayData = datas => {
     const dataContainer = document.getElementById('data-container');
     const seeMore = document.getElementById('seeMore');
-    if(datas.length > 6){
+    if (datas.length > 6) {
         datas = datas.slice(0, 6);
         seeMore.classList.remove('d-none')
     }
-    else{
+    else {
         seeMore.classList.add('d-none');
     }
     datas.forEach(data => {
@@ -49,9 +49,58 @@ const togleSpinner = isLoading => {
     if (isLoading) {
         lodarSection.classList.remove('d-none')
     }
-    else{
+    else {
         lodarSection.classList.add('d-none');
     }
 }
+document.getElementById('btn-see-More').addEventListener('click', function () {
+    const loadData = async () => {
+        const url = `https://openapi.programming-hero.com/api/ai/tools`
+        const res = await fetch(url);
+        const data = await res.json();
+        displayData(data.data.tools);
+    }
+    const displayData = datas => {
+        const dataContainer = document.getElementById('data-container');
+        dataContainer.innerText = "";
+        const seeMore = document.getElementById('seeMore');
+        if (datas.length > 6) {
+            seeMore.classList.add('d-none')
+        }
+        else {
+            seeMore.classList.remove('d-none');
+        }
+        datas.forEach(data => {
+            const dataDiv = document.createElement('div');
+            dataDiv.classList.add('col');
+            dataDiv.innerHTML = `
+            <div class="card p-3 h-100">
+            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Features</h5>
+                <ol>
+                    <li>${data}</li>
+                    <li>${data}</li>
+                    <li>${data}</li>
+                </ol>
+            <hr>
+            <h5>${data.name}</h5>
+            <div class="row justify-content-between">
+                <div class="col-4">
+                <p><i class="fa-solid fa-calendar-days"></i> ${data.published_in}</p>
+                </div>
+                <div class="col-2">
+                <button><i class="fa-sharp fa-solid fa-arrow-right"></i></button>
+                </div>
+            </div>
+            </div>
+        </div>
+            `;
+            dataContainer.appendChild(dataDiv);
+            togleSpinner(false);
+        });
+    };
+    loadData();
+})
 togleSpinner(true);
 loadData();
